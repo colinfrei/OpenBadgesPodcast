b4f8b7edbad033343e863d840ae5663c7a0d3972
Colin Frei <colin.frei@liip.ch>
2013-12-17 06:46:14 +0100

    Don’t output the (internal) id of the feed item

--- src/ColinFrei/OpenBadgesPodcastBundle/Entity/PodcastItem.php
+++ src/ColinFrei/OpenBadgesPodcastBundle/Entity/PodcastItem.php
@@ -20,6 +20,8 @@ class PodcastItem
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     * @Serializer\Exclude
      */
     private $id;
 
