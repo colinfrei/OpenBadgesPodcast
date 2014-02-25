4fa472a33d80cd70c3103203da7bc73fcd1042f0
Colin Frei <colin@colinfrei.com>
2014-02-25 16:08:28 +0100

    More probably useless stuff that Sensiolabs insight highlighted

--- src/ColinFrei/OpenBadgesPodcastBundle/DependencyInjection/Configuration.php
+++ src/ColinFrei/OpenBadgesPodcastBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('colin_frei_openbadges_podcast');
+        $treeBuilder->root('colin_frei_openbadges_podcast');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
